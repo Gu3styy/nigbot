52aecb4c10b4690721e944b4887aac2675440344
Gu3styy <maximusfrapus123@gmail.com>
2025-12-18 10:51:15 +0100

    Update start.py

--- start.py
+++ start.py
@@ -190,6 +190,20 @@ async def ping(interaction: discord.Interaction):
     latency = round(bot.latency * 1000)
     await interaction.response.send_message(f"Pong! {latency}ms")
 
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot alive"
+
+def run_web():
+    app.run(host="0.0.0.0", port=8080)
+
+threading.Thread(target=run_web).start()
+
 
 token = os.environ.get('DISCORD_BOT_TOKEN')
 if not token:
